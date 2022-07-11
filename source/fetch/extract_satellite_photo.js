e7503b1f007b0a60f286ed06bdab462b4694191a
David Lukert <david.lukert@rwth-aachen.de>
2022-07-11 16:32:01 +0200

    Init packages, sketch-canvas, xlsx dependencies

---
+++ source/fetch/extract_satellite_photo.js
@@ -0,0 +1,15 @@
+const XLSX = require('xlsx') ;
+
+const parseExcel = (filename) => {
+
+    const excelData = XLSX.readFile(filename);
+
+    return Object.keys(excelData.Sheets).map(name => ({
+        name,
+        data: XLSX.utils.sheet_to_json(excelData.Sheets[name]),
+    }));
+};
+
+parseExcel("query_citiesatnight.xlsx").forEach(element => {
+    console.log(element.data);
+});
